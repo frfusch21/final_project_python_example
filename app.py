9aed7c763c71cc965931ec0434142a807fc211a0
frfusch21 <reviewfarmei20@gmail.com>
2022-09-26 21:17:56 +0700

    Tambah Path

--- app.py
+++ app.py
@@ -10,5 +10,29 @@ def index():
         f.close()
     return render_template("index.html", fileData = fileData)
 
+@app.route("/about", methods=["POST", "GET"])
+def about():
+    # fileData = 0
+    # with open('database.json', 'r') as f:
+    #     fileData = json.load(f)
+    #     f.close()
+    return render_template("about.html")
+
+@app.route("/service", methods=["POST", "GET"])
+def service():
+    # fileData = 0
+    # with open('database.json', 'r') as f:
+    #     fileData = json.load(f)
+    #     f.close()
+    return render_template("service.html")
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    # fileData = 0
+    # with open('database.json', 'r') as f:
+    #     fileData = json.load(f)
+    #     f.close()
+    return render_template("contact.html")
+
 if __name__ == "__main__":
   app.run()
